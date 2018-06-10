de14c0673a9c28a0fa7fddeff774fde9a409ff19
VeronikaNechpai <39963557+VeronikaNechpai@users.noreply.github.com>
2018-06-10 18:51:16 +0300

    Add files via upload

--- Task1_13_5.java
+++ Task1_13_5.java
@@ -1,28 +1,32 @@
 package by.epam.lesson3.hometask3;
+
 import java.util.Scanner;
 
 public class Task1_13_5 {
-	public static void main (String[] args) {
-		int n=10;
-		Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		int n = 10;
+		Scanner sc = new Scanner(System.in);
 		String max = "", min = "", str = "";
-		for (int i=0;i<n;i++) {
-		 System.out.print("> ");
-		 if (sc.hasNextLine()) {
-			 str=sc.nextLine();
-			 if (i==0) {
-				 min=str;
-				 if (str.length()>max.length()){
-			maxLgth(str);}
-			 } else if (str.length()<min.length())
-				 min=str;
-		 }
+		for (int i = 0; i < n; i++) {
+			System.out.print("> ");
+			if (sc.hasNextLine()) {
+				str = sc.nextLine();
+				if (i == 0) {
+					min = str;
+					if (str.length() > max.length()) {
+						maxLgth(str);
+					}
+				} else if (str.length() < min.length())
+					min = str;
+			}
 		}
-		 System.out.println("max string = " + max + " length=" + max.length());
-		 System.out.println("min string = " + min + " length=" + min.length());}
-		public static void maxLgth(String str) {
-			String max="";
-			 max=str;
-		} return max;
+		System.out.println("max string = " + max + " length=" + max.length());
+		System.out.println("min string = " + min + " length=" + min.length());
 	}
-			
+
+     public static String maxLgth(String str) {
+	    String max="";
+	     max=str;
+	     return max;
+      }
+}
